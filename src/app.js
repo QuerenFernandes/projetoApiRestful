aae4356e5cf6e20ebef16d554015a4d97a43e637
Queren Hapuque <queren.fernandes@yahoo.com>
2023-07-07 11:12:52 -0300

    implementando o put

--- src/app.js
+++ src/app.js
@@ -3,6 +3,9 @@ import express from "express";
 //instanciando o express
 const app = express();
 
+//código para que a requisição entenda que é em json
+app.use(express.json());
+
 //criando o array de livro
 const livros = [
     {id: 1, "titulo": "Senhor dos anéis"},
@@ -19,9 +22,28 @@ app.get('/livros', (req, res) => {
     res.status(200).json(livros);
 })
 
+//procurando um livro pelo id 
+app.get('/livros/:id', (req, res) => {
+    let index = buscaLivro(req.params.id);
+    res.json(livros[index]);
+})
+
+
+//enviando um cadastro novo
 app.post('/livros', (req, res) => {
     livros.push(req.body);
     res.status(201).send('Livro cadastrado.')
 })
 
+//alterando um cadastro 
+app.put('/livros/:id', (req, res) => {
+    let index = buscaLivro(req.params.id);
+    livros[index].titulo = req.body.titulo;
+    res.json(livros);
+})
+
+//função para localizar o livro pelo id
+function buscaLivro(id){
+    return livros.findIndex(livro=> livro.id == id);
+}
 export default app
